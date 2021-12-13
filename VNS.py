Commit 13.12. (Permutations implemented, distance calculation fixed)

VNS.py
@@ -1,6 +1,10 @@
+import random
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+from dubins_path_planning import dubins_path_planning
 from heading import Heading
 from location import Location
 from path import Path
@@ -32,21 +36,57 @@ def target_locations_from_df(target_locations_df):
     return target_locations
 
 
+def generate_cutpoints(list_to_cut, number_of_cutpoints):
+    """Generates distinct and sorted cutpoints on list that are random but not allowed to be the first index"""
+    while True:
+        r = []
+        for i in range(number_of_cutpoints):
+            r.append(random.randint(1, len(list_to_cut)-1))  # only the index 0 is not allowed
+        if len(set(r)) == number_of_cutpoints:
+            break
+    r.sort()
+    return r
+
+
 class VNS:
-    def __init__(self, T_max, radius, stepsize, target_locations_df, m, l_max=2, start_idy=0, end_idy=0):
+    def __init__(self, T_max, curvature, stepsize, target_locations_df, m, l_max=2, start_idy=0, end_idy=0):
         self.T_max = T_max
-        self.radius = radius
+        self.curvature = curvature
         self.stepsize = stepsize
         self.target_locations = target_locations_from_df(target_locations_df)
+        self.target_locations_idys = [location.idy for location in self.target_locations]
         self.m = m
         self.heading_samples = generate_heading_samples(m)
         self.l_max = l_max
         self.start_idy = start_idy
         self.end_idy = end_idy
-        self.distance_dict = DubinsDistanceDict(curvature=self.radius,
+        print("Filling distance dict")
+        self.distance_dict = DubinsDistanceDict(curvature=self.curvature,
                                                 stepsize=self.stepsize,
                                                 locations=self.target_locations,
                                                 headings=self.heading_samples)
+
+    def variable_neighborhood_search(self, number_of_iterations):
+        print("Calculating reachable locations")
+        reachable_locations = self.get_reachable_locations()
+        print("Creating initial path")
+        path = self.create_initial_path(reachable_locations)
+        print(f"Initial path with reward {path.reward} and distance {path.distance} built")
+        iteration = 1
+        while not iteration >= number_of_iterations:
+            l = 1
+            while l <= self.l_max:
+                path1 = self.shake(path, l)
+                path2 = self.local_search(path1, l)
+                if path2.distance <= self.T_max & path2.reward >= path.reward:
+                    path = path2
+                    l = 1
+                else:
+                    l = l + 1
+            iteration = iteration + 1
+            print(f"Iteration {iteration}, Current best path (Reward: {path.reward}, Distance: {path.distance})")
+        print([location.idy for location in path.locations])
+        return path
 
     def get_reachable_locations(self):
         """qi ∈ Sr ⇔ (Ld(q1,qi)+ Ld(qi,qn )) ≤ Tmax for any combination of sampled heading angles (θ1,θi,θn ).
@@ -80,7 +120,9 @@ class VNS:
         start_location = next((location for location in self.target_locations if location.idy == self.start_idy), None)
         end_location = next((location for location in self.target_locations if location.idy == self.end_idy), None)
         locations_to_add = [location for location in reachable_locations if location.idy not in [self.start_idy, self.end_idy]]
-        path = Path(locations=[start_location, end_location],
+        initial_permutation = [self.start_idy, self.end_idy] + [idy for idy in self.target_locations_idys if idy not in [self.start_idy, self.end_idy]]
+        path = Path(permutation=initial_permutation,
+                    locations=[start_location, end_location],
                     heading_samples=self.heading_samples,
                     distance_dict=self.distance_dict,
                     T_max=self.T_max)
@@ -90,63 +132,132 @@ class VNS:
     def shake(self, path, l):
         path1 = path
         if l == 1:
-            path1 = path_move(path)
+            path1 = self.path_move(path)
         elif l == 2:
-            path1 = path_exchange(path)
+            path1 = self.path_exchange(path)
         return path1
 
-    def local_search(path,l):
-        path1 = path
-        len(path.locations)**2
+    def path_move(self, path):
+        r = generate_cutpoints(path.permutation, 2)
+        location_idys_to_be_moved = path.permutation[r[0]:r[1]+1]
+        location_idys_to_be_kept = [path.permutation[idx] for idx in range(0, len(path.permutation)) if idx not in range(r[0], r[1]+1)]
+        o = generate_cutpoints(location_idys_to_be_kept, 1)
+        for idx in range(0, len(location_idys_to_be_moved)):
+            location_idys_to_be_kept.insert(o[0]+idx, location_idys_to_be_moved[idx])
+        permutation1 = location_idys_to_be_kept
+        locations1 = self.get_locations_from_permutation(permutation=permutation1)
+        path1 = Path(permutation=permutation1,
+                     locations=locations1,
+                     heading_samples=self.heading_samples,
+                     distance_dict=self.distance_dict,
+                     T_max=self.T_max)
+        return path1
+
+    def path_exchange(self, path):
+        r = generate_cutpoints(path.permutation, 4)
+        location_idys_to_be_kept1 = path.permutation[0:r[0]]
+        location_idys_to_be_moved1 = path.permutation[r[0]:r[1]+1]
+        location_idys_to_be_kept2 = path.permutation[r[1]+1:r[2]]
+        location_idys_to_be_moved2 = path.permutation[r[2]:r[3]+1]
+        location_idys_to_be_kept3 = path.permutation[r[3]+1:len(path.permutation)]
+        permutation1 = location_idys_to_be_kept1 + location_idys_to_be_moved2 + location_idys_to_be_kept2 + location_idys_to_be_moved1 + location_idys_to_be_kept3
+        locations1 = self.get_locations_from_permutation(permutation=permutation1)
+        path1 = Path(permutation=permutation1,
+                     locations=locations1,
+                     heading_samples=self.heading_samples,
+                     distance_dict=self.distance_dict,
+                     T_max=self.T_max)
+        return path1
+
+    def local_search(self, path, l):
+        number_of_iterations = len(path.locations)**2
+        current_best_path = path
         if l == 1:
-            path1 = one_point_move(path)
+            for i in range(number_of_iterations):
+                path1 = self.one_point_move(path)
+                if path1.reward > current_best_path.reward:
+                    current_best_path = path1
         elif l == 2:
-            path1 = one_point_exchange(path)
+            for i in range(number_of_iterations):
+                path1 = self.one_point_exchange(path)
+                if path1.reward > current_best_path.reward:
+                    current_best_path = path1
+        return current_best_path
+
+    def one_point_move(self, path):
+        r = generate_cutpoints(path.permutation, 1)
+        permutation1 = [path.permutation[idx] for idx in range(0, len(path.permutation)) if idx not in r]
+        r_insert = generate_cutpoints(permutation1, 1)
+        permutation1.insert(r_insert[0], path.permutation[r[0]])
+        locations1 = self.get_locations_from_permutation(permutation=permutation1)
+        path1 = Path(permutation=permutation1,
+                     locations=locations1,
+                     heading_samples=self.heading_samples,
+                     distance_dict=self.distance_dict,
+                     T_max=self.T_max)
         return path1
 
-    def variable_neighborhood_search(self):
-        reachable_locations = self.get_reachable_locations()
-        path = self.create_initial_path(reachable_locations)
-        # stopping_criterion = False
-        # while not stopping_criterion:
-        #     l = 1
-        #     while l <= l_max:
-        #         path1 = shake(path, l)
-        #         path2 = localSearch(path1, l)
-        #         if path2.length <= T_max & path2.get_reward() >= path.get_reward():
-        #             path = path2
-        #             l = 1
-        #         else:
-        #             l = l + 1
-        print([location.idy for location in path.locations])
-        return path
+    def one_point_exchange(self, path):
+        r = generate_cutpoints(path.permutation, 2)
+        location_idys_to_be_kept1 = path.permutation[0:r[0]]
+        location_idys_to_be_moved1 = path.permutation[r[0]:r[0]+1]
+        location_idys_to_be_kept2 = path.permutation[r[0]+1:r[1]]
+        location_idys_to_be_moved2 = path.permutation[r[1]:r[1]+1]
+        location_idys_to_be_kept3 = path.permutation[r[1]+1:len(path.permutation)]
+        permutation1 = location_idys_to_be_kept1 + location_idys_to_be_moved2 + location_idys_to_be_kept2 + location_idys_to_be_moved1 + location_idys_to_be_kept3
+        locations1 = self.get_locations_from_permutation(permutation=permutation1)
+        path1 = Path(permutation=permutation1,
+                     locations=locations1,
+                     heading_samples=self.heading_samples,
+                     distance_dict=self.distance_dict,
+                     T_max=self.T_max)
+        return path1
 
-    # def variable_neighborhood_search2(T_max, radius, stepsize, locations, m, l_max=2, start_index=0, end_index=0):
-    #     heading_samples = generate_heading_samples(m)
-    #     distance_dict = DubinsDistanceDict(curvature=radius, stepsize=stepsize)
-    #     distance_dict.fill(locations, heading_samples)
-    #     reachable_locations = get_reachable_locations(T_max=T_max,
-    #                                                   locations=locations,
-    #                                                   heading_samples=heading_samples,
-    #                                                   distance_dict=distance_dict,
-    #                                                   start_index=start_index,
-    #                                                   end_index=end_index)
-    #     path = create_initial_path(reachable_locations, T_max, start_index, end_index)
-    #     stopping_criterion = False
-    #     while not stopping_criterion:
-    #         l = 1
-    #         while l <= l_max:
-    #             path1 = shake(path, l)
-    #             path2 = localSearch(path1, l)
-    #             if path2.length <= T_max & path2.get_reward() >= path.get_reward():
-    #                 path = path2
-    #                 l = 1
-    #             else:
-    #                 l = l + 1
-    #     return path
-    #
+    def get_locations_from_permutation(self, permutation):
+        location_idys_path = permutation[permutation.index(self.start_idy):permutation.index(self.end_idy)+1]
+        locations = []
+        for location_idy in location_idys_path:
+            locations.append(next((location for location in self.target_locations if location.idy == location_idy), None))
+        return locations
 
+    def plot(self, path):
+        path_x = []
+        path_y = []
+        for idx in range(len(path.locations)-1):
+            location1 = path.locations[idx]
+            heading1 = path.optimal_headings[idx]
+            location2 = path.locations[idx+1]
+            heading2 = path.optimal_headings[idx+1]
+            # start = [location1.x, location1.y, heading1.angle]
+            # goal = [location2.x, location2.y, heading2.angle]
+            # query_result = self.distance_dict.DubinsPlanner.query(start=start, goal=goal)
+            # path_array_i = [[a[0], a[1]] for a in query_result[0]]
+            # path_array = path_array + path_array_i
+            path_x_i, path_y_i, path_yaw_i, mode_i, lengths_i = dubins_path_planning(s_x=location1.x,
+                                                                                     s_y=location1.y,
+                                                                                     s_yaw=heading1.angle,
+                                                                                     g_x=location2.x,
+                                                                                     g_y=location2.y,
+                                                                                     g_yaw=heading2.angle,
+                                                                                     curvature=self.curvature,
+                                                                                     step_size=self.stepsize)
+            length_i = sum(lengths_i)
+            print(f"Length from {location1.idy} to {location2.idy}: {length_i}")
+            path_x = np.concatenate((path_x, path_x_i), axis=None)
+            path_y = np.concatenate((path_y, path_y_i), axis=None)
+        # x_path = [a[0] for a in path_array]
+        # y_path = [a[1] for a in path_array]
+        fig, ax = plt.subplots()
+        x_targets = [location.x for location in self.target_locations]
+        y_targets = [location.y for location in self.target_locations]
+        r_targets = [location.r for location in self.target_locations]
+        sc = ax.scatter(x_targets, y_targets, label="Target locations", c=r_targets)
+        ax.plot(path_x, path_y, label="Dubins path")
+        ax.legend()
+        ax.set_aspect("equal")
+        cbar = plt.colorbar(sc, label="Reward")
+        fig.show()
+        return fig, ax
 
-
-    #plot_rewards1 = plt.scatter(TestSet.x,TestSet.y, s = TestSet.r*2, c = "b")
+    # plot_rewards1 = plt.scatter(TestSet.x,TestSet.y, s = TestSet.r*2, c = "b")
 

path.py
@@ -1,17 +1,18 @@
-from scipy.sparse.csgraph import shortest_path
 import networkx as nx
 
-
 class Path:
-    def __init__(self, locations, heading_samples, distance_dict, T_max):
+    def __init__(self, permutation, locations, heading_samples, distance_dict, search_graph, T_max):
+        self.permutation = permutation
         self.locations = locations
-        #self.reward = self.get_reward()
+        self.reward = self.get_reward()
         self.heading_samples = heading_samples
         self.distance_dict = distance_dict
+        self.search_graph = search_graph
         self.optimal_headings = self.calculate_optimal_headings()
         self.distance = self.get_distance()
         self.T_max = T_max
         self.is_feasible = self.distance <= self.T_max
+
 
     def add_new_locations(self, locations_to_add):
         """Adds locations iteratively in order of decreasing added reward per added distance"""
@@ -24,11 +25,15 @@ class Path:
                 # Create new path path1 with location_to_add at given index
                 locations1 = self.locations.copy()
                 locations1.insert(index, location_to_add)
-                path1 = Path(locations=locations1,
+                permutation1 = self.permutation.copy()
+                permutation1.remove(location_to_add.idy)
+                permutation1.insert(index, location_to_add.idy)
+                path1 = Path(permutation=permutation1,
+                             locations=locations1,
                              heading_samples=self.heading_samples,
                              distance_dict=self.distance_dict,
                              T_max=self.T_max)
-                reward_per_distance_added1 = (path1.get_reward() - self.get_reward())/(path1.get_distance() - self.get_distance())
+                reward_per_distance_added1 = (path1.reward - self.reward)/(path1.distance - self.distance)
                 if (reward_per_distance_added1 > current_reward_per_distance_added) & path1.is_feasible:
                     current_reward_per_distance_added = reward_per_distance_added1
                     current_best_path = path1
@@ -36,7 +41,7 @@ class Path:
                     continue
         if best_location_to_add is not None:
             locations_to_add.remove(best_location_to_add)
-            print(str(best_location_to_add.idy) + " removed")
+            print(str(best_location_to_add.idy) + " added")
             print("new locations_to_add" + str([la.idy for la in locations_to_add]))
             if len(locations_to_add) >= 1:
                 current_best_path = current_best_path.add_new_locations(locations_to_add)
@@ -59,7 +64,8 @@ class Path:
         return distance
 
     def calculate_optimal_headings(self):
-        G = self.generate_search_graph()
+        #G = self.generate_search_graph()
+        G = self.search_graph
         shortest_path_in_g = nx.shortest_path(G,
                                               source="start",
                                               target="end",
@@ -101,6 +107,7 @@ class Path:
                        v_of_edge="end",
                        weight=0)
         return G
+
 
 
 